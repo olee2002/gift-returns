9d9476dcb03c40936a6c9c62f001acac40e9c9f7
dphurley <hurley.dannyp@gmail.com>
2017-12-21 15:30:53 -0500

    Added new Gift form.

--- controllers/giftsController.js
+++ controllers/giftsController.js
@@ -3,6 +3,38 @@ const router = express.Router({mergeParams: true})
 
 const User = require('../db/models/User')
 
+router.get('/new', (request, response) => {
+  const userId = request.params.userId
+  const storeId = request.params.storeId
+
+  User.findById(userId).then((user) => {
+    const store = user.stores.id(storeId)
+
+    response.render('gifts/new', {
+      userId,
+      store
+    })
+  })
+})
+
+router.post('/', (request, response) => {
+  const userId = request.params.userId
+  const storeId = request.params.storeId
+
+  const newGift = request.body
+  console.log(newGift)
+
+  User.findById(userId).then((user) => {
+    const store = user.stores.id(storeId)
+    store.giftsToReturn.push(newGift)
+
+    return user.save()
+  })
+  .then(() => {
+    response.redirect(`/users/${userId}/stores/${storeId}`)
+  })
+})
+
 router.get('/:giftId', (request, response) => {
   const userId = request.params.userId
   const storeId = request.params.storeId
